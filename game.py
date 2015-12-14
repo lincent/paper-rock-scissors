0c2dd10e2933aa15afa98c46f99f0c9630531a3f
lincent <lukevincent2@gmail.com>
2015-12-14 21:06:23 +0000

    Commit final

--- game.py
+++ game.py
@@ -1,12 +1,14 @@
 from random import randint
 
+rps = ('rock', 'paper', 'scissors')
+
 def user_input():
     a = 0
 
     while a < 5:
         user = input("What will you play? (Paper, Rock, or Scissors):  ").lower()
-        if user in ('rock', 'paper', 'scissors'):
-            return user
+        if user in rps:
+            return rps.index(user)
         else:
             print("Invalid input")
             a += 1
@@ -15,52 +17,37 @@ def user_input():
     exit()
 
 def comp_input():
-    num = randint(1,3)
-    if num == 1:
-        return 'rock'
-    elif num == 2:
-        return 'paper'
-    return 'scissors'
-    
-def decide_winner(user, comp):
-    if user == comp:
-        return 'draw'
-    elif user == 'paper' and comp == 'rock':
-        return 'win'
-    elif user == 'paper' and comp == 'scissors':
-        return 'lose'
-    elif user == 'rock' and comp == 'scissors':
-        return 'win'
-    elif user == 'rock' and comp == 'paper':
-        return 'lose'
-    elif user == 'scissors' and comp == 'paper':
-        return 'win'
-    elif user == 'scissors' and comp == 'rock':
-        return 'lose'
+    num = randint(0,2)
+    return num
 
+
+def decide_winner(user, comp):
+    res = user - comp
+    if res == -1 or res == 2:
+        return 'lose'
+    elif res == -2 or res == 1:
+        return 'win'
+    return 'draw'
 
 rounds = int(input("How many rounds would you like to play? "))
 print()
 
-a = 1
-comp_score = 0
-user_score = 0
+r_counter, comp_score, user_score = 1, 0, 0
 
-while a <= rounds:
-    print("Round " + str(a))
+while r_counter <= rounds:
+    print("Round " + str(r_counter))
     user = user_input()
     comp = comp_input()
-
-    print("You played " + user + " : Computer plays " + comp)
-
     game_res = decide_winner(user, comp)
+
+    print("You played " + rps[user] + " : Computer plays " + rps[comp])
     
     if game_res == 'win':
         user_score += 1
     elif game_res == 'lose':
         comp_score += 1
     elif game_res == 'draw':
-        a -= 1
+        r_counter -= 1
     
     print("You " + game_res + " the round")
     print("You " + str(user_score) + " : Computer " + str(comp_score))
@@ -69,10 +56,10 @@ while a <= rounds:
     if rounds % 2 != 0:
         if comp_score >= rounds / 2 or user_score >= rounds / 2:
             print("We have a winner!")
-            a = rounds + 1
+            r_counter = rounds + 1
     else:
         if comp_score > rounds / 2 or user_score > rounds / 2:
             print("We have a winner!")
-            a = rounds + 1
-    a += 1
+            r_counter = rounds + 1
+    r_counter += 1
 print("Final Score: You " + str(user_score) + " : Computer " + str(comp_score))
